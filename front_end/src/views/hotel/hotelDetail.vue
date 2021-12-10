5fde9501b72a8c492bfaa7b45ed2490b2cf6604a
Pole <2567712208@qq.com>
2021-12-10 20:44:57 +0800

    add:地图路径

--- front_end/src/views/hotel/hotelDetail.vue
+++ front_end/src/views/hotel/hotelDetail.vue
@@ -38,7 +38,7 @@
         <!--            </div>-->
         <!--          </div>-->
         <!--        </div>-->
-        <div style="max-width: 570px; margin-bottom: 10px">
+        <div style="; margin-bottom: 10px">
           <span style="font-weight: bold; font-size: 20px">{{ currentHotelInfo.name }}</span>
           <span class="hotelScore">{{ currentHotelInfo.rate.toFixed(1) }}</span>
           <br>
@@ -54,8 +54,12 @@
           <div class="hotelInfo">
             <a-icon type="info-circle" theme="twoTone" style="margin-right: 15px; font-size: large"/>
             <span>{{ currentHotelInfo.description }}</span>
+            <a-button type="link" style="float: right" @click="changeCheckPath">如何到达</a-button>
+
           </div>
         </div>
+        <!--          </div>-->
+        <!--        </div>-->
 
         <baidu-map id="map" :center="{lng: getHotelCoordinate()[0], lat: getHotelCoordinate()[1]}" :zoom="20"
                    style="height: 350px; width: 570px; display: inline-block; float: right; border: solid #9b9b9b 1px;
@@ -64,8 +68,30 @@
                      animation="BMAP_ANIMATION_BOUNCE"></bm-marker>
           <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
         </baidu-map>
-        <!--        <div id="map" style="height: 350px; width: 600px; display: inline-block; float: right"></div>-->
 
+        <a-modal :body-style="bodystyle"
+                 :visible="this.pathCheckModalVisable"
+                 centered="true"
+                 :footer="null"
+                 width="100%"
+                 @cancel="changeCheckPath"
+        >
+          <div>
+            <baidu-map id="map2" :center="{lng: hotelLongitude, lat: hotelLatitude}" :zoom="20"
+                       style="height: 350px; width: 100%; display: inline-block; ">
+              <bm-marker :position="{lng: hotelLongitude, lat: hotelLatitude}"
+                         animation="BMAP_ANIMATION_BOUNCE"></bm-marker>
+              <bm-marker :position="{lng: 118.97024, lat: 32.12009}" animation="BMAP_ANIMATION_BOUNCE"></bm-marker>
+              <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+              <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+              <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true"
+                              :autoLocation="true"></bm-geolocation>
+              <bm-transit start="南京大学鼓楼校区" :end="currentHotelInfo.address" :auto-viewport="true"
+                          location=""></bm-transit>
+            </baidu-map>
+
+          </div>
+        </a-modal>
         <a-divider></a-divider>
 
         <a-tabs>
@@ -73,7 +99,9 @@
             <RoomList :rooms="currentHotelInfo.rooms"></RoomList>
           </a-tab-pane>
           <a-tab-pane tab="用户评价" key="2">
+
             <CommentList :comments="commentList">></CommentList>  <!--新增-->
+
           </a-tab-pane>
         </a-tabs>
       </div>
@@ -81,109 +109,124 @@
   </a-layout>
 </template>
 <script>
-  import {mapGetters, mapActions, mapMutations} from 'vuex'
-  import RoomList from './components/roomList'
-  import CommentList from './components/commentList'
+import {mapGetters, mapActions, mapMutations} from 'vuex'
+import RoomList from './components/roomList'
+import CommentList from './components/commentList'
 
-  export default {
-    name: 'hotelDetail',
-    components: {
-      RoomList,
-      CommentList,//新增
-    },
-    data() {
-      return {
-        starDic: ['One', 'Two', 'Three', 'Four', 'Five'],
-        // hotelLongitude: 118.97024,  // 酒店经度
-        // hotelLatitude: 32.12009,    // 酒店纬度
-      }
-    },
-    computed: {
-      ...mapGetters([
-        'currentHotelInfo',
-        'commentList'//新增
-      ])
-    },
-    mounted() {
-      this.set_currentHotelId(Number(this.$route.params.hotelId));
-      this.getHotelById();
-      this.getHotelComments();//新增
-    },
-    beforeRouteUpdate(to, from, next) {
-      this.set_currentHotelId(Number(to.params.hotelId));
-      this.getHotelById();
-      this.getHotelComments();//新增
-      next()
-    },
-    methods: {
-      ...mapMutations([
-        'set_currentHotelId',
-        'set_hotelComments',//新增
-      ]),
-      ...mapActions([
-        'getHotelById',
-        "getHotelComments",//新增
-      ]),
+export default {
+  name: 'hotelDetail',
+  components: {
+    RoomList,
+    CommentList,//新增
+  },
+  data() {
+    return {
+      starDic: ['One', 'Two', 'Three', 'Four', 'Five'],
+      // hotelLongitude: 118.97024,  // 酒店经度
+      // hotelLatitude: 32.12009,    // 酒店纬度
+      hotelLongitude: 118.97024,  // 酒店经度
+      hotelLatitude: 32.12009,    // 酒店纬度
+      pathCheckModalVisable: false,
+      bodystyle: {
+        height: '100%',
+      },
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'currentHotelInfo',
+      'commentList'//新增
+    ])
+  },
+  mounted() {
+    this.set_currentHotelId(Number(this.$route.params.hotelId));
+    this.getHotelById();
+    this.getHotelComments();//新增
+    this.getHotelCoordinate()
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.set_currentHotelId(Number(to.params.hotelId));
+    this.getHotelById();
+    this.getHotelComments();//新增
+    next()
+  },
+  methods: {
+    ...mapMutations([
+      'set_currentHotelId',
+      'set_hotelComments',//新增
+    ]),
+    ...mapActions([
+      'getHotelById',
+      "getHotelComments",//新增
+    ]),
 
-      // 根据酒店地址获取经纬度
-      getHotelCoordinate() {
-        console.log(this.currentHotelInfo.id, this.currentHotelInfo.name);
-        const hotelCoordinates = {
-          5: [118.798432, 32.045335], // 大地君悦酒店
-          6: [118.733997, 31.810431], // 山林悦民居
-          7: [118.81311, 31.971188], // 南京樱悦美宿酒店(南京南站店)
-          8: [118.809051, 31.971437], // 清沐铂金酒店
-        };
-        // console.log(hotelCoordinates[this.currentHotelInfo.id]);
-        return hotelCoordinates[this.currentHotelInfo.id];
-      }
+    // 根据酒店地址获取经纬度
+    getHotelCoordinate() {
+      console.log(this.currentHotelInfo.id, this.currentHotelInfo.name);
+      const hotelCoordinates = {
+        5: [118.798432, 32.045335], // 大地君悦酒店
+        6: [118.733997, 31.810431], // 山林悦民居
+        7: [118.81311, 31.971188], // 南京樱悦美宿酒店(南京南站店)
+        8: [118.809051, 31.971437], // 清沐铂金酒店
+      };
+      // console.log(hotelCoordinates[this.currentHotelInfo.id]);
+      return hotelCoordinates[this.currentHotelInfo.id];
+    },
+    changeCheckPath() {
+      this.pathCheckModalVisable = !this.pathCheckModalVisable;
     }
   }
+}
 </script>
 <style scoped lang="less">
-  .hotelDetailCard {
-    padding: 50px 50px;
-  }
+.hotelDetailCard {
+  padding: 50px 50px;
+}
 
-  .hotelScore {
-    background-color: rgb(24, 144, 255);
-    padding: 6px 10px;
-    border-radius: 8px;
-    color: white;
-    font-weight: bold;
-    font-size: large;
-    margin: 0 0 0 70px;
-  }
+.hotelScore {
+  background-color: rgb(24, 144, 255);
+  padding: 6px 10px;
+  border-radius: 8px;
+  color: white;
+  font-weight: bold;
+  font-size: large;
+  margin: 0 0 0 70px;
+}
 
-  .hotelInfo {
-    margin-top: 10px;
-  }
+.hotelInfo {
+  margin-top: 10px;
+}
 
-  .hotel-info {
+.hotel-info {
+  display: flex;
+  align-items: stretch;
+  justify-content: flex-start;
+
+  .info {
+    padding: 10px 0;
     display: flex;
-    align-items: stretch;
-    justify-content: flex-start;
+    flex-direction: column;
+    margin-left: 20px;
 
-    .info {
-      padding: 10px 0;
+    .items {
       display: flex;
-      flex-direction: column;
-      margin-left: 20px;
+      /*align-items: center;*/
+      margin-bottom: 10px;
 
-      .items {
-        display: flex;
-        /*align-items: center;*/
-        margin-bottom: 10px;
+      .label {
+        margin-right: 10px;
+        font-size: 18px;
+      }
 
-        .label {
-          margin-right: 10px;
-          font-size: 18px;
-        }
+      .value {
+        margin-right: 15px
+      }
 
-        .value {
-          margin-right: 15px
-        }
+      .pathModal {
+        width: 60%;
+        height: 50%;
       }
     }
   }
+}
 </style>
